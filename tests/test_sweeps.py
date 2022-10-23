bfcb17454f00dc37ca1e9f9f3c1b081ff5c19f43
Nicole Bussola <nicole.bussolaceradini@gmail.com>
2022-10-24 00:28:03 +0200

    fix test_sweeps

--- tests/test_sweeps.py
+++ tests/test_sweeps.py
@@ -1,3 +1,6 @@
+import os
+from pathlib import Path
+
 import pytest
 
 from tests.helpers.run_if import RunIf
@@ -6,18 +9,34 @@ from tests.helpers.run_sh_command import run_sh_command
 startfile = "src/train.py"
 overrides = ["logger=[]"]
 
+TEST_DIR = os.path.dirname(Path(__file__).absolute())
+
+train_data_dir = os.path.join(TEST_DIR, "fixtures/train_cells")
+test_data_dir = os.path.join(TEST_DIR, "fixtures/test_cells")
+valid_data_dir = os.path.join(TEST_DIR, "fixtures/valid_cells")
+
+dir_conf = [
+    f"++datamodule.train_data_dir={train_data_dir}",
+    f"++datamodule.test_data_dir={test_data_dir}",
+    f"++datamodule.valid_data_dir={valid_data_dir}",
+]
+
 
 @RunIf(sh=True)
 @pytest.mark.slow
 def test_experiments(tmp_path):
     """Test running all available experiment configs with fast_dev_run=True."""
-    command = [
-        startfile,
-        "-m",
-        "experiment=glob(*)",
-        "hydra.sweep.dir=" + str(tmp_path),
-        "++trainer.fast_dev_run=true",
-    ] + overrides
+    command = (
+        [
+            startfile,
+            "-m",
+            "experiment=glob(*)",
+            "hydra.sweep.dir=" + str(tmp_path),
+            "++trainer.fast_dev_run=true",
+        ]
+        + overrides
+        + dir_conf
+    )
     run_sh_command(command)
 
 
@@ -25,13 +44,17 @@ def test_experiments(tmp_path):
 @pytest.mark.slow
 def test_hydra_sweep(tmp_path):
     """Test default hydra sweep."""
-    command = [
-        startfile,
-        "-m",
-        "hydra.sweep.dir=" + str(tmp_path),
-        "model.optimizer.lr=0.005,0.01",
-        "++trainer.fast_dev_run=true",
-    ] + overrides
+    command = (
+        [
+            startfile,
+            "-m",
+            "hydra.sweep.dir=" + str(tmp_path),
+            "model.optimizer.lr=0.005,0.01",
+            "++trainer.fast_dev_run=true",
+        ]
+        + overrides
+        + dir_conf
+    )
 
     run_sh_command(command)
 
@@ -40,17 +63,21 @@ def test_hydra_sweep(tmp_path):
 @pytest.mark.slow
 def test_hydra_sweep_ddp_sim(tmp_path):
     """Test default hydra sweep with ddp sim."""
-    command = [
-        startfile,
-        "-m",
-        "hydra.sweep.dir=" + str(tmp_path),
-        "trainer=ddp_sim",
-        "trainer.max_epochs=3",
-        "+trainer.limit_train_batches=0.01",
-        "+trainer.limit_val_batches=0.1",
-        "+trainer.limit_test_batches=0.1",
-        "model.optimizer.lr=0.005,0.01,0.02",
-    ] + overrides
+    command = (
+        [
+            startfile,
+            "-m",
+            "hydra.sweep.dir=" + str(tmp_path),
+            "trainer=ddp_sim",
+            "trainer.max_epochs=3",
+            "+trainer.limit_train_batches=1",
+            "+trainer.limit_val_batches=1",
+            "+trainer.limit_test_batches=1",
+            "model.optimizer.lr=0.005,0.01,0.02",
+        ]
+        + overrides
+        + dir_conf
+    )
     run_sh_command(command)
 
 
@@ -66,5 +93,8 @@ def test_optuna_sweep(tmp_path):
         "hydra.sweeper.n_trials=10",
         "hydra.sweeper.sampler.n_startup_trials=5",
         "++trainer.fast_dev_run=true",
+        f"++datamodule.train_data_dir={train_data_dir}",
+        f"++datamodule.test_data_dir={test_data_dir}",
+        f"++datamodule.valid_data_dir={valid_data_dir}",
     ] + overrides
     run_sh_command(command)
